ebb477240daf827b42680a8fb5348c492d4925a8
enzoesper <enzoesper7@gmail.com>
2024-09-19 18:16:24 -0300

    adding scroll

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const { chromium } = require("playwright");
 
 (async () => {
-  const browser = await chromium.launch({ headless: true });
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://www.perramus.com.ar/hombre");
+
+  await page.waitForTimeout(2000);
+
+  await page.evaluate(() => {
+    window.scrollBy(0, window.innerHeight * 3);
+  });
+
+  await page.waitForTimeout(2000);
 
   const disc = await page.evaluate(() => {
     const spans = document.querySelectorAll(
